7f18f0fe320963c03b545f5e15fb4a5aa7c3c732
brianchesko <cheskobrian@gmail.com>
2021-03-09 17:27:21 -0500

    minor refactor

--- Project_Beachworm/frontend/src/api/axiosApi.js
+++ Project_Beachworm/frontend/src/api/axiosApi.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const baseUrl = 'http://127.0.0.1:8000/api/';
+
 const axiosInstance = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api/',
+    baseURL: baseUrl,
     timeout: 5000,
     headers: {
         'Authorization': "JWT " + localStorage.getItem('access_token'),
