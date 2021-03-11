800db8951160d8bc04977ea482728ab6fa5ba4d4
maxlliiw <willie1191@gmail.com>
2021-03-11 16:30:19 -0500

    Appearence Updates Updated Navbar Updated Footer Updated outer-wrapper (background gradient) Current Issues: Page Resizing

--- Project_Beachworm/frontend/src/pages/nav/Navbar.js
+++ Project_Beachworm/frontend/src/pages/nav/Navbar.js
@@ -1,31 +1,97 @@
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './NavBar.css';
+
 
 function Navbar(props) {
 
   const { menuList, setSearchField, submitSearch } = props;
+  const [click, setClick] = useState(false);
+  const [dropdown, setDropdown] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  const onMouseEnter = () => {
+    if (window.innerWidth < 960) {
+      setDropdown(false);
+    } else {
+      setDropdown(true);
+    }
+  };
+
+  const onMouseLeave = () => {
+    if (window.innerWidth < 960) {
+      setDropdown(false);
+    } else {
+      setDropdown(false);
+    }
+  };
    
   return (
-
-      <div className='nav-bar'>
-        <ul className='nav-bar-items'>
-          {           
-            Object.entries(menuList).map( (menuPair , index) => {
-                const href = menuPair[0];
-                const pageName = menuPair[1];
-                return(
-                  <Link to={href} key={index}>{pageName}</Link>
-                )
-            })
-          }
-        </ul>
-        <div className='nav-bar-search-bar'>
-          <form onSubmit={submitSearch}>
-            <input className='nav-bar-search-input' type='text' id='search' name='search'  placeholder='Search' onChange={ (e) => setSearchField(e.target.value) } />
-            <button className='nav-bar-search-button' type='submit'> Search </button>
-          </form>
-        </div>
-      </div>
+      <>
+        <nav className='navbar'>
+          <Link to='/landing' className='navbar-logo' onClick={closeMobileMenu}>
+            BW
+          </Link>
+          <div className='menu-icon' onClick={handleClick}>
+          <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+          </div>
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            <li className='nav-item'>
+              <Link 
+                to='/' 
+                className='nav-links' 
+                onClick={closeMobileMenu}
+              >
+                home
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link 
+                to='/explore'
+                className='nav-links'
+                onClick={closeMobileMenu}
+              >
+                explore
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link
+                to='/playlist'
+                className='nav-links'
+                onClick={closeMobileMenu}
+              >
+                playlist[temp]
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link
+                to='/landing'
+                className='nav-links'
+                onClick={closeMobileMenu}
+              >
+                landing[temp]
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link
+                to='/questionnaire'
+                className='nav-links'
+                onClick={closeMobileMenu}
+              >
+                questionnaire[temp]
+              </Link>
+            </li>
+          </ul>
+          <div className='nav-bar-search-bar'>
+            <form onSubmit={submitSearch}>
+              <input className='nav-bar-search-input' type='text' id='search' name='search'  placeholder='Search' onChange={ (e) => setSearchField(e.target.value) } />
+              <button className='nav-bar-search-button' type='submit'> Search </button>
+            </form>
+          </div>
+        </nav>
+      </>
     )
 
   }
