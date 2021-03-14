3c36944c8a448f26e96049ebfdaeb653948f6eaf
brianchesko <cheskobrian@gmail.com>
2021-03-14 19:36:53 -0400

    fix bug with scroll text for changing children, now works when the child text changes but its ref does not. also fixes all warnings in the file

--- Project_Beachworm/frontend/src/pages/playingToolbar/ScrollText.js
+++ Project_Beachworm/frontend/src/pages/playingToolbar/ScrollText.js
@@ -1,81 +1,95 @@
-import { useState, useEffect, Fragment } from 'react';
+import { useState, useEffect, useCallback, Fragment } from 'react';
 import './ScrollText.css';
 import { Transition } from 'react-transition-group';
 
 function ScrollText(props) {
-  const [scrolling, setScrolling] = useState(false);
-  const [textRef, setTextRef] = useState(null);
-  const [wrapperRef, setWrapperRef] = useState(null);
-  const [bulletRef, setBulletRef] = useState(null);
   const { rampMillis, decayMillis, speed, children } = props;
-  const [scrollWidth, setScrollWidth] = useState(0);
-  const [shouldScroll, setShouldScroll] = useState(false);
-  const [duration, setDuration] = useState(0);
-  const [scheduled, setScheduled] = useState([]);
+  const [scrolling, setScrolling] = useState(false); // whether the wrapper is CURRENTLY scrolling
+  const [childRef, setChildRef] = useState(null); // ref for the element that scrolling is based upon
+  const [wrapperRef, setWrapperRef] = useState(null); // ref for the wrapping parent element
+  const [bulletRef, setBulletRef] = useState(null); // ref for the delimiter between copies of the text
+  const [childWidth, setChildWidth] = useState(0); // width of target children
+  const [wrapperWidth, setWrapperWidth] = useState(0); // width of parent. scrolling occurs if this is < child
+  const [shouldScroll, setShouldScroll] = useState(false); // whether or not to scroll at all, based on child and wrapper widths
+  const [duration, setDuration] = useState(0); // calculated duration of a single scroll event
+  const [scheduled, setScheduled] = useState([]); // list of scheduled events used to change scrolling status
+
+  const scheduleNew = useCallback((callback, timeout) => {
+    const newTimeout = setTimeout(callback, timeout);
+    setScheduled(scheduled => [...scheduled, newTimeout])
+  }, []);
+
+  const clearScheduled = useCallback(() => {
+    if (scheduled.length) {
+      scheduled.forEach(x => clearInterval(x));
+      setScheduled([]);
+    }
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+  // theoretically this should have a dependency on scheduled, but
+  // that causes an infinite loop since other methods then have a dependency
+  // on this function
+
+  const scheduleInitial = useCallback(() => {
+    setShouldScroll(true);
+    clearScheduled();
+    scheduleNew(() => {
+      setScrolling(true);
+    }, rampMillis);
+  }, [scheduleNew, clearScheduled, rampMillis]);
+
+  // every time something important changes, (re)start scrolling, if necessary
+  useEffect(() => {
+    if (wrapperWidth < childWidth) {
+      // speed of 0 corresponds to 1 cps, 100 corresponds to 20 cps.  
+      const charactersPerSec = Math.floor(1 + (19 * speed / 100));
+      const pixelsPerSec = charactersPerSec * 12; // assuming font size 12
+      const newDuration = 1000 * childWidth / pixelsPerSec;
+      setDuration(newDuration);
+      scheduleInitial();
+    } else {
+      setShouldScroll(false);
+      clearScheduled();
+    }
+  }, [childWidth, wrapperWidth, scheduleInitial, clearScheduled, speed, childRef, wrapperRef]);
+
+  // lint disabled because we WANT this to run every render.
+  // since the ref doesn't actually change when the song changes,
+  // we need to manually set the widths which will call a re-render if different
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(() => {
+    if (childRef) {
+      setChildWidth(childRef.scrollWidth);
+    }
+    if (wrapperRef) {
+      setWrapperWidth(wrapperRef.clientWidth);
+    }
+  });
+
+  const handleFinishedScrolling = () => {
+    scheduleNew(() => {
+      clearScheduled();
+      setScrolling(true);
+    }, decayMillis + rampMillis);
+  };
+
+  const handleRestartScrolling = () => {
+    scheduleNew(() => {
+      clearScheduled();
+      setScrolling(false);
+    }, duration);
+  };
 
   const defaultStyle = {
     transition: `left ${duration}ms linear`,
     left: 0,
   };
 
-  const scheduleNew = (callback, timeout) => {
-    const newTimeout = setTimeout(callback, timeout);
-    setScheduled([...scheduled, newTimeout])
-  };
-
   const transitionStyles = shouldScroll ? {
-    entering: { left: -(scrollWidth + (bulletRef ? bulletRef.scrollWidth : 12)) },
-    entered: { left: -(scrollWidth + (bulletRef ? bulletRef.scrollWidth : 12)), transition: `left 0ms linear` },
+    entering: { left: -(childWidth + (bulletRef ? bulletRef.scrollWidth : 12)) },
+    entered: { left: -(childWidth + (bulletRef ? bulletRef.scrollWidth : 12)), transition: `left 0ms linear` },
     exiting: { left: 0, transition: `left 0ms linear` },
     exited: { left: 0, transition: `left 0ms linear` },
   } : {};
-
-  // on mount and unmount, clear all timers
-  useEffect(() => {
-    scheduled.forEach(x => clearInterval(x));
-  }, []);
-
-  useEffect(() => {
-    if (textRef) {
-      setScrollWidth(textRef.scrollWidth);
-      // speed of 0 corresponds to 1 cps, 100 corresponds to 20 cps.
-      const charactersPerSec = Math.floor(1 + (19 * speed / 100));
-      const pixelsPerSec = charactersPerSec * 12; // assuming font size 12
-      const newDuration = 1000 * textRef.scrollWidth / pixelsPerSec;
-      setDuration(newDuration);
-
-      if (wrapperRef && wrapperRef.clientWidth < textRef.scrollWidth) {
-        setShouldScroll(true);
-
-        // setTimeout(() => {
-        //   console.log('Starting initial scroll for text ' + textRef.innerHTML);
-        //   setScrolling(true);
-        // }, rampMillis);
-        scheduleNew(() => {
-          console.log('Starting initial scroll for text ' + textRef.innerHTML);
-          setScrolling(true);
-        }, rampMillis);
-      }
-    }
-  }, [textRef, wrapperRef, rampMillis, speed]);
-
-  const handleFinishedScrolling = () => {
-    // setTimeout(() => {
-    //   setScrolling(true);
-    // }, decayMillis + rampMillis);
-    scheduleNew(() => {
-      setScrolling(true);
-    }, decayMillis + rampMillis);
-  };
-
-  const handleRestartScrolling = () => {
-    // setTimeout(() => {
-    //   setScrolling(false);
-    // }, duration);
-    scheduleNew(() => {
-      setScrolling(false);
-    }, duration);
-  };
 
   return (
     <Transition in={scrolling} timeout={{
@@ -92,7 +106,7 @@ function ScrollText(props) {
             ...defaultStyle,
             ...transitionStyles[state]
           }} className="scroll-wrapper" ref={e => setWrapperRef(e)}>
-            <span className="child-wrapper" ref={e => setTextRef(e)}>
+            <span className="child-wrapper" ref={e => setChildRef(e)}>
               {children}
             </span>
             {shouldScroll && <Fragment>
