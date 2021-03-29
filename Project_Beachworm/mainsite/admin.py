c2d2b743a6a9f1612cd25b02d9f4c6b1877c8de8
Joseph Altmann <altman47@students.rowan.edu>
2021-03-29 15:38:52 -0400

    made the id visible in admin pages

--- Project_Beachworm/mainsite/admin.py
+++ Project_Beachworm/mainsite/admin.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from .models import *
 
+class IDAdmin(admin.ModelAdmin):
+    readonly_fields = ('id',)
+
 admin.site.register(Profile)
-admin.site.register(Playlist)
-admin.site.register(Radio)
-admin.site.register(UserArtistSeed)
-admin.site.register(UserGenreSeed)
-admin.site.register(UserSongPlay)
-admin.site.register(UserPlaylistPlay)
+admin.site.register(Playlist, IDAdmin)
+admin.site.register(Radio, IDAdmin)
+admin.site.register(UserArtistSeed, IDAdmin)
+admin.site.register(UserGenreSeed, IDAdmin)
+admin.site.register(UserSongPlay, IDAdmin)
+admin.site.register(UserPlaylistPlay, IDAdmin)
 admin.site.register(Song)
