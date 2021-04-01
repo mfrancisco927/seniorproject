f149e4b85fce867369cf3e5c466a8441d9b1e638
brianchesko <cheskobrian@gmail.com>
2021-04-01 11:03:54 -0400

    restore refresh token migration

--- Project_Beachworm/mainsite/migrations/0003_auto_20210313_2228.py
+++ Project_Beachworm/mainsite/migrations/0003_auto_20210313_2228.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('mainsite', '0002_playlist_description'),
+        ('mainsite', '0002_profile_refresh_token'),
     ]
 
     operations = [
