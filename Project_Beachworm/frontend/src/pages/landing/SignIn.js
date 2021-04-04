bff51391dd2f6a3ba2f6e06a5160351df62c354a
maxlliiw <willie1191@gmail.com>
2021-04-04 14:38:36 -0400

    sign up

--- Project_Beachworm/frontend/src/pages/landing/SignIn.js
+++ Project_Beachworm/frontend/src/pages/landing/SignIn.js
@@ -1,131 +1,151 @@
+import React from "react";
 import { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { refreshToken } from './../../api/authenticationApi';
 import { createUser } from './../../api/userApi';
 import { useAuth } from './../../hooks/authHooks';
 import { useSpotifySdk } from './../../hooks/spotifyHooks';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import TextField from "material-ui/TextField";
+import "./landing.css";
+const validator = require("validator");
 
-function SignIn() {
-  const auth = useAuth();
-  const spotify = useSpotifySdk();
-  const history = useHistory();
-  const { redirect } = history.location.state || {}; // location.state;
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [errorText, setErrorText] = useState();
-  const [access, setAccess] = useState(localStorage.getItem('access_token'))
-  const [refresh, setRefresh] = useState(localStorage.getItem('refresh_token'))
 
-  // not needed for production, delete
-  const refreshTokens = () => {
-      refreshToken().then(value => {
-          setErrorText();
-      }).catch(reason => {
-          console.log('Token refresh failed with reason: ', reason)
-          setErrorText(reason);
-      });
-      
-      updateTokenVars();
-  }
+function SignInForm () {
+    const auth = useAuth();
+    const spotify = useSpotifySdk();
+    const history = useHistory();
+    const { redirect } = history.location.state || {};
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [access, setAccess] = useState(localStorage.getItem('access_token'))
+    const [refresh, setRefresh] = useState(localStorage.getItem('refresh_token'))
+    const [error, setErrorText] = useState('');
 
-  // not needed for production, delete
-  // move the removeItem calls to a log out functionality if we want to keep that
-  const deleteTokens = () => {
-      auth.signOut(updateTokenVars);
-  }
+    const handleChange = (stateSetter) => {
+        console.log(email + ' ' + username + ' ' + password);
+        return (event) => stateSetter(event.target.value);
+        
+    };
 
-  // not needed for production, delete
-  const updateTokenVars = () => {
-      setAccess(localStorage.getItem('access_token'));
-      setRefresh(localStorage.getItem('refresh_token'));
-  }
+    const updateTokenVars = () => {
+        setAccess(localStorage.getItem('access_token'));
+        setRefresh(localStorage.getItem('refresh_token'));
+    }
 
-  const handleChange = (stateSetter) => {
-      return (event) => stateSetter(event.target.value);
-  }
+    const refreshTokens = () => {
+        refreshToken().then(value => {
+            setErrorText();
+        }).catch(reason => {
+            console.log('Token refresh failed with reason: ', reason)
+            setErrorText(reason);
+        });
+        
+        updateTokenVars();
+    }
 
-  // todo: remove set error text stuff, instead do actual form validation.
-  // if an error is still returned by the signIn method, display a 
-  // top-level error on the page and let the user know
-  const handleSubmit = (event) => {
-      event.preventDefault();
+    const deleteTokens = () => {
+        auth.signOut(updateTokenVars);
+    }
 
-      auth.signIn(username, password).then(value => {
-          updateTokenVars();
-          spotify.clearAll();
-          setErrorText();
-          if (redirect) {
-            history.replace(redirect);
-          }
-      }).catch(reason => {
-          console.log('Sign in failed with reason ', reason)
-          setErrorText(reason);
-      });
-      event.preventDefault();
-  }
+    const validateForm = () => {
+        if(password !== confirmPassword){
+            alert('Passwords do not match')
+            console.log('password validation fail')
+        } else if (!validator.isEmail(email)) {
+            alert('Please enter a valid email address')
+            console.log('email validation fail')
+        } else {
+            createNewUser(email, username, password);
+        }
+    };
 
-  const createNewUser = (event) => {
-      const rng = Math.floor(Math.random() * 1000);
-      const email = 'example' + rng + '@example.com';
-      const user = 'testuser' + rng;
-      const pass = 'securePassword' + rng;
-      createUser(email, user, pass).then(value => {
-          console.log(value);
-          alert('Created user with username ' + user +  ' and password ' + pass)
-      }).catch(reason => {
-          console.log('new user rejected', reason);
-      });
-      event.preventDefault();
-  }
+    const createNewUser = (email, username, password) => {
+        createUser(email, username, password).then(value => {
+            console.log(value);
+            signInUser();
+            alert('Created user with username ' + username +  ' and password ' + password)
+        }).catch(reason => {
+            console.log('new user rejected', reason);
+        });
+    };
 
-  const signInForm = (
-      <form onSubmit={handleSubmit}>
-          <label>
-              Username:
-              <input type="text"
-                  placeholder="Enter username"
-                  name="uname"
-                  required
-                  onChange={handleChange(setUsername)}
-              />
-          </label>
-          <label>
-              Password:
-              <input type="password"
-                  placeholder="Enter password"
-                  name="psw"
-                  required
-                  onChange={handleChange(setPassword)}
-              />
-          </label>
-          <input type="submit" value="Submit" />
-      </form>
-  )
+    const signInUser = () => {
+        auth.signIn(username, password).then(value => {
+            updateTokenVars();
+            spotify.clearAll();
+            setErrorText();
+            if (redirect) {
+              history.replace(redirect);
+            }
+        }).catch(reason => {
+            console.log('Sign in failed with reason ', reason)
+            setErrorText(reason);
+        });
+    }
 
-  return (
-      <div>
-          <h3>Testing notes</h3>
-          <p>
-              Users must be created in the db to test here! As long as you have a
-              superuser, which you can make with
-          </p>
-          <code>
-              python manage.py createsuperuser
-          </code>
-          <p>you can then create as many accounts as you want through
-              <a href="localhost:8000/admin"> /admin </a>
-              once signed in
-          </p>
-          <p>After clearing local storage, user must sign in again to regain access token</p>
-          {signInForm}
-          <button onClick={refreshTokens}>Refresh</button>
-          <button onClick={deleteTokens} >Clear local storage</button>
-          <button onClick={createNewUser} >Create user test</button>
-          <p>Access token: {access}</p>
-          <p>Refresh token: {refresh}</p>
-          <p>{errorText ? 'Error text: ' + errorText : ''}</p>
-      </div>
-  );
+    const StyledButton = withStyles({
+        root: {
+          background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+          borderRadius: 3,
+          border: 0,
+          color: 'white',
+          height: 48,
+          padding: '0 30px',
+          boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+        },
+        label: {
+          textTransform: 'capitalize',
+        },
+      })(Button);
+
+    return (
+        <div className="loginBox">
+        <h1>Sign Up</h1>
+
+        <form onSubmit={''}>
+            <TextField
+                name="username"
+                floatingLabelText="user name"
+                value={username}
+                onChange={handleChange(setUsername)}
+            />
+            <br />
+            <TextField
+                name="email"
+                floatingLabelText="email"
+                value={email}
+                onChange={handleChange(setEmail)}
+            />
+            <br />
+            <TextField
+                name="password"
+                floatingLabelText="password"
+                value={password}
+                onChange={handleChange(setPassword)}
+            />
+            <br />
+            <TextField
+                name="pwconfirm"
+                floatingLabelText="confirm password"
+                value={confirmPassword}
+                onChange={handleChange(setConfirmPassword)}
+            />
+            <br />
+            <StyledButton type='submit' onClick={validateForm}>
+                SUBMIT
+            </StyledButton>
+
+        </form>
+        <p>
+            Aleady have an account? <br />
+            <a href="/">Log in here</a>
+        </p>
+        </div>
+    );
 }
 
-export default SignIn;
+export default SignInForm;
