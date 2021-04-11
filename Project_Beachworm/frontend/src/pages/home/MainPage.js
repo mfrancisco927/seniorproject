d96eca240502c0345360949bf16ef914bcc41c52
Joseph Altmann <altman47@students.rowan.edu>
2021-04-11 15:59:40 -0400

    Added responsiveness for home page

--- Project_Beachworm/frontend/src/pages/home/MainPage.js
+++ Project_Beachworm/frontend/src/pages/home/MainPage.js
@@ -5,11 +5,14 @@ import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 import { getHomeRecommendations } from './../../api/recommendationApi';
 import LoadingImage from '../loading.svg';
 import useRadioLoaders from '../../hooks/radioLoaders';
+import { useWindowDimensions, SCREEN_SIZE } from './../../hooks/responsiveHooks';
 
 function MainPage() {
   const loader = useRadioLoaders();
   const [data, setData] = useState({});
   const [loaded, setLoaded] = useState(false);
+  const { width } = useWindowDimensions();
+  const isMobile = width <= SCREEN_SIZE.SMALL;
 
   useEffect(() => {
     getHomeRecommendations().then((data) => {
@@ -20,7 +23,7 @@ function MainPage() {
     );
   }, [])
 
-  return (
+  return !isMobile? (
     <Fragment>
       <div className='song-page-wrapper'>
       { !loaded ? 
@@ -72,6 +75,57 @@ function MainPage() {
       </div>
       </Fragment>
     
+    ) : (
+      <Fragment>
+      <div className='song-page-wrapper'>
+      { !loaded ? 
+        (<div className='loading-img-wrapper'><img className='loading-img' src={LoadingImage} alt='Loading'/></div> ):
+        (
+          <div>
+          <SongRow title='Recommended Tracks' 
+            getItems={ () => data.tracks}
+            onItemClick={song => loader.loadSongRadio(song)}
+            getImageCallback={(item) => {
+              if(item.album.images.length !== 0){
+                  return item.album.images[0].url;
+                }else{
+                  return 'https://media.pitchfork.com/photos/5a71df0d85ed77242d8f1252/1:1/w_320/jpegmafiaveteran.jpg';
+              }
+            }
+            }
+            getTitle={ (item) => { return (<h2 className='artist-title'>{item.name}</h2>)}}            
+            getSubtitle={ (item) => {return (<h3 className='artist-subtitle' >{item.artists.map(artist => artist.name).join(', ')}</h3>) }}
+            />
+          <SongRow title='Recommended Artists' 
+            getItems={ () => data.artists}
+            onItemClick={artist => loader.loadArtistRadio(artist)}
+            getImageCallback={(item) => {
+              if(item.images.length !== 0){
+                  return item.images[0].url;
+                }else{
+                  return 'https://media.pitchfork.com/photos/5a71df0d85ed77242d8f1252/1:1/w_320/jpegmafiaveteran.jpg';
+              }
+            }
+          } 
+            getTitle={ (item) => { return <h2 className='artist-title'>{item.name}</h2> }}
+            getSubtitle={ (item) => ''}
+            />
+          <SongRow title='Recommended Genres' 
+            getItems={ () => data.genres}
+            onItemClick={item => loader.loadGenreRadio({
+              id: item,
+              name: item,
+            })}
+            getImageCallback={ item => 'https://media.pitchfork.com/photos/5a71df0d85ed77242d8f1252/1:1/w_320/jpegmafiaveteran.jpg'}
+            getTitle={ (item) => {return (<h2 className='artist-title'>{item}</h2>)}}
+            getSubtitle={ item => ''}
+              />
+                  
+          </div>
+        )
+      }
+      </div>
+      </Fragment>
     );
 }
 
@@ -79,6 +133,8 @@ function SongRow(props){
   const {title, getImageCallback, getItems, getTitle, getSubtitle, onItemClick} = props;
   const songBoxRef = useRef({});
   const MAX_ITEMS_SHOWN = 10;
+  const { width } = useWindowDimensions();
+  const isMobile = width <= SCREEN_SIZE.SMALL;
   
   const moveRow = (direction) => {
     if(direction === 'left'){
@@ -102,7 +158,7 @@ function SongRow(props){
     checkForOverflow();
   }, [])
 
-    return (
+    return !isMobile? (
       <div className='group-wrapper' >
           <div className='group-header'><h2>{title}</h2></div>
               <div className='songs-buttons-wrapper'>
@@ -121,6 +177,28 @@ function SongRow(props){
                 }
                 </div>
                 <ArrowForwardIosIcon fontSize='large' className='pan pan-right' onClick={() => moveRow('right')}/>
+
+              
+            </div>
+        </div>
+    ) : 
+    ( 
+      <div className='group-wrapper-sm' >
+          <div className='group-header-sm'><h2>{title}</h2></div>
+                <div className='songs-wrapper-sm' ref={songBoxRef}>
+                { 
+                  getItems().slice(0,Math.min(MAX_ITEMS_SHOWN, getItems().length)).map((item, index) => {
+                    return (
+                      <div className='song-wrapper-sm' key={`wrapper-index-${index}`} onClick={ () => onItemClick(item)}>
+                          <img className='song-img-sm' src={getImageCallback(item)} alt='hello!'/> 
+                          { getTitle(item) }
+                          { getSubtitle(item) }
+                      </div>
+                    );
+                  })
+                }
+  
+            
 
               
             </div>
